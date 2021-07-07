b12a9d007193d29f6b4ceec3a51796a521354dbd
micheleh <ehud.michelson@hpe.com>
2021-07-07 18:01:16 +0300

    For testing purposes add an option to output junit report

--- src/test/java/MyBankAppTest.java
+++ src/test/java/MyBankAppTest.java
@@ -5,5 +5,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin="com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results\\TestMyBankApp_OctaneGherkinResults.xml",
 tags="not @exclude")
+//@CucumberOptions(plugin = "junit:target/cucumber-reports/cucumber-junit.xml")
 public class MyBankAppTest {
 }
